f75a6a26bab4d2ce7909787c6c437a78c10f8151
TheLostFrequency <85657945+TheLostFrequency@users.noreply.github.com>
2026-09-15 23:53:26 -0400

    Update Supabase client initialization code

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,9 +1,7 @@
 // SUPABASE CLIENT INITIALIZATION
 const SUPABASE_URL = "https://nmiodppvxqpzfrideduv.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5taW9kcHB2eHFwemZyaWRlZHV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4ODk5MjgsImV4cCI6MjEwNDQ2NTkyOH0.ZoHpE2QIv-E7ofcf9os0oTZzC8B6ak3f3ZkCWG6-yDY";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5taW9kcHB2eHFwemZlZHV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4ODk5MjgsImV4cCI6MjEwNDQ2NTkyOH0.ZoHpE2QIv-E7ofcf9os0oTZzC8B6ak3f3ZkCWG6-yDY";
 
-// Ensure url is trimmed of trailing slashes or rest endpoints
 const cleanUrl = SUPABASE_URL.replace(/\/rest\/v1\/?$/, '').replace(/\/$/, '');
 
-// Initializing global Supabase client instance
-const supabaseClient = window.supabase.createClient(cleanUrl, SUPABASE_ANON_KEY);
+export const supabase = window.supabase ? window.supabase.createClient(cleanUrl, SUPABASE_ANON_KEY) : null;
