435d8b1f0c2d79605228e2d30914b5d71e10d0a3
TheLostFrequency <85657945+TheLostFrequency@users.noreply.github.com>
2026-09-18 02:34:20 -0400

    Set room transition test to roughly four seconds

--- js/frequency-app.js
+++ js/frequency-app.js
@@ -157,7 +157,7 @@ nav.forEach(button => {
                 document.body.classList.remove('room-arriving');
                 roomTransitionBusy = false;
             }, 2000);
-        }, 650);
+        }, 2000);
     });
 });
 
