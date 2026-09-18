4508b68d33b2f7d9ab7403d7bd7f0cecaea8623e
TheLostFrequency <85657945+TheLostFrequency@users.noreply.github.com>
2026-09-18 02:32:50 -0400

    Slow down room transition timing

--- js/frequency-app.js
+++ js/frequency-app.js
@@ -156,8 +156,8 @@ nav.forEach(button => {
             window.setTimeout(() => {
                 document.body.classList.remove('room-arriving');
                 roomTransitionBusy = false;
-            }, 450);
-        }, 420);
+            }, 650);
+        }, 650);
     });
 });
 
